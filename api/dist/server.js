deaccc22c45f1816111291fa58b6b342a87dd36c
dimasaptr <dimasaptr@gmail.com>
2026-04-17 18:25:13 +0700

    chore: major cleanup of repository pollution and redundant gitignore

--- api/dist/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`=========================================`);
-    console.log(`🚀 Server running on port ${PORT}`);
-    console.log(`🔗 URL: http://localhost:${PORT}`);
-    console.log(`=========================================`);
-});
